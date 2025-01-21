c4f7ed29fa1370b699a1a9586b29cad0579a5e8a
Ani <a.gvin3@gmail.com>
2025-01-21 20:00:40 +0400

    core/added fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,8 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        publicSans: ["Public Sans", "sans-serif"],
       },
     },
   },
