09bb394eea409c5c2ab269781fd9cdbb7b9a4462
Ani <a.gvin3@gmail.com>
2025-01-23 21:48:46 +0400

    feat/deletedcirclePropsFunc

--- app/index.ts
+++ app/index.ts
@@ -10,25 +10,6 @@ import activetrans from '../public/assets/activeTransactiond.svg';
 import activebudget from '../public/assets/activebudgets.svg';
 import activPots from '../public/assets/activePots.svg';
 import activeBils from '../public/assets/activebills.svg';
-import circleImg from '@/public/assets/circle.svg';
-
-interface CircleProps {
-  color?: string; // Optional color prop
-}
-
-function Circle({ color = '#277C78' }) {
-  return (
-    <svg
-      width='16'
-      height='16'
-      viewBox='0 0 16 16'
-      fill='none'
-      xmlns='http://www.w3.org/2000/svg'
-    >
-      <circle cx='8' cy='8' r='8' fill={color} />
-    </svg>
-  );
-}
 
 export {
   loginImg,
