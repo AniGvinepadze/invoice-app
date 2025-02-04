8b819fbd9f45b90b73e887606a330d17560293d3
BTavkhelidze <beqatavkhelidze4@gmail.com>
2025-02-04 16:06:43 +0400

    feat: create authorization and authentication
    adding auth

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,8 +1,38 @@
-import HomePage from "@/app/components/organsms/HomePage/HomePage";
+'use client';
+import HomePage from '@/app/components/organsms/HomePage/HomePage';
+import axios from 'axios';
+import { getCookie } from 'cookies-next';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [user, setUser] = useState();
+  const router = useRouter();
+
+  const getCurrentUser = async (token: string) => {
+    try {
+      const response = await axios.get(
+        'http://localhost:3001/auth/current-user',
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setUser(response.data);
+    } catch (err) {
+      router.push('/login');
+    }
+  };
+
+  useEffect(() => {
+    const token = getCookie('accessToken');
+    getCurrentUser(token as string);
+  }, []);
+
+  if (!user) return null;
   return (
-    <div className="max-w-[1440px] w-full">
+    <div className='max-w-[1440px] w-full'>
       <HomePage />
     </div>
   );
--- app/(dashboard)/pots/page.tsx
+++ app/(dashboard)/pots/page.tsx
@@ -3,15 +3,68 @@ import BudgetModal from '@/app/components/organsms/BudgetModal/BudgetModal';
 import React, { useEffect, useState } from 'react';
 import data from '@/data.json';
 import PotsContent from '@/app/components/moleculs/pots/potsContent';
+import { getCookie } from 'cookies-next';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+
+interface IPots {
+  title: string;
+  content: string;
+}
+
+const fetchPots = async (token: string): Promise<IPots[]> => {
+  let url = `http://localhost:3001/pots`;
+
+  const res = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!res.ok) throw new Error('Failed to fetch data, response is not OK');
+
+  const data = await res.json();
+
+  return data;
+};
 function Pots() {
+  const router = useRouter();
   const [pots, setPots] = useState(data.pots || []);
+
+  const [user, setUser] = useState();
+
+  const getCurrentUser = async (token: string) => {
+    try {
+      const response = await axios.get(
+        'http://localhost:3001/auth/current-user',
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setUser(response.data);
+    } catch (err) {
+      router.push('/login');
+    }
+  };
+
+  const token = getCookie('accessToken');
   useEffect(() => {
-    console.log(pots, 'set');
+    getCurrentUser(token as string);
+  }, []);
+
+  useEffect(() => {
+    const potsFetch = async () => {
+      const res = await fetchPots(token as string);
+      console.log(res, 'pots');
+    };
+    potsFetch();
   }, [pots]);
+
+  if (!user) return null;
+
   return (
-
     <div className='px-10  pt-8 pb-[48px] w-full overflow-x-hidden overflow-scroll h-screen'>
-
       <div className='flex justify-between items-center'>
         <h1 className='font-bold text-3xl'>Pots</h1>
         <BudgetModal handleSetPots={setPots} />
